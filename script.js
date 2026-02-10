b3f2bad83fe1cdf3d03fe6c7c763f6baafdc87e0
nikhileswar234 <nikhileswar234@gmail.com>
2026-02-10 21:56:29 +0530

    Update fetch URL in generate function

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function generate() {
     duration: duration.value
   };
 
-  fetch("http://127.0.0.1:5000/generate-plan", {
+  fetch("https://ai-diet-planner-zw7x.onrender.com", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data)
@@ -36,3 +36,4 @@ function generate() {
     result.innerHTML = html;
   });
 }
+

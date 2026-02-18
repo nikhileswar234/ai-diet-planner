e34f7f2aec20730faf6477489aa2fe70c8c925f4
nikhileswar234 <nikhileswar234@gmail.com>
2026-02-19 00:59:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from google import genai
-from datetime import datetime
+
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
@@ -83,3 +83,4 @@ def generate():
 
 if __name__ == "__main__":
     app.run()
+

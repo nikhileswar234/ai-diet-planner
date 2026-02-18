b385922494cb5fab838dfa53d79a441d07eb49e0
nikhileswar234 <nikhileswar234@gmail.com>
2026-02-19 02:26:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -103,7 +103,15 @@ function downloadPDF() {
 }
 function toggleDarkMode() {
   document.body.classList.toggle("dark-mode");
+
+  if (document.body.classList.contains("dark-mode")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
 }
+
+
 
 
 
